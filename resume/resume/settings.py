1d6bb549d8d148b90b91b99b00c6e62424c2ae8c
JavaGhost1 <Secret09!>
2024-01-23 09:50:27 +0300

    modern look

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'fJlhqZMeWXnCAD2NHCX0',
-        'HOST': 'containers-us-west-35.railway.app',
-        'PORT': '6709',
-    }  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'fJlhqZMeWXnCAD2NHCX0',
+#         'HOST': 'containers-us-west-35.railway.app',
+#         'PORT': '6709',
+#     }  
+# }
 
 
 # Password validation
