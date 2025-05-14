50bb4bd8674d200036fb6ac827c98d76431c859f
javaGhost1 <dennismurage97@gmail.com>
2025-05-14 17:20:21 +0300

    improved website design, navbar positioning and included an update tech stack

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -85,17 +85,13 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'fJlhqZMeWXnCAD2NHCX0',
-        'HOST': 'containers-us-west-35.railway.app',
-        'PORT': '6709',
-    }  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
